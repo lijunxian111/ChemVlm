1b51e15adbbda8ca930c24afb980241fcc6524ba
lijunxian <1713546501@qq.com>
2024-07-26 13:28:43 +0800

    add gpt-4o eval

--- evaluation/test_smiles_ocr.py
+++ evaluation/test_smiles_ocr.py
@@ -14,10 +14,17 @@ import json
 import time
 import random
 from tqdm import tqdm
+from openai import OpenAI
+import openai
+from PIL import Image
+
 
 from time import sleep
 
 dashscope.api_key = 'sk-eea7c876461747c5a6eebe0531164767' #qwen API-KEY
+
+
+openai.api_key = 'sk-Kf4b9EQQn4z3SzMK05CfB280FdBf4fE0A74b45A7E69f4bA4'
 
 
 def sim_cal_metric(input_smiles:str, gt_smiles:str):
@@ -61,32 +68,55 @@ def call_qwen(question:str):
         ))
     return full_content
 
-def call_qwen_multimodal(image_path, text):
+def call_multimodal(model_name, image_path, text):
     """
-    call qwen-vl-v1 
+    call qwen-vl-v1, gpt-4 and other 
     """
     local_file_path = image_path
-    messages = [{
-        'role': 'system',
-        'content': [{
-            'text': 'You are a helpful assistant.'
+    if model_name == "qwen-vl":
+        messages = [{
+            'role': 'system',
+            'content': [{
+                'text': 'You are a helpful assistant.'
+            }]
+        }, {
+            'role':
+            'user',
+            'content': [
+                {
+                    'image': 'file://'+local_file_path
+                },
+                {
+                    'text': text
+                },
+            ]
         }]
-    }, {
-        'role':
-        'user',
-        'content': [
-            {
-                'image': 'file://'+local_file_path
-            },
-            {
-                'text': text
-            },
-        ]
-    }]
-    #response = MultiModalConversation.call(model=MultiModalConversation.Models.qwen_vl_chat_v1, messages=messages)
-    response = MultiModalConversation.call(model='qwen-vl-plus', messages=messages)
+        #response = MultiModalConversation.call(model=MultiModalConversation.Models.qwen_vl_chat_v1, messages=messages)
+        response = MultiModalConversation.call(model='qwen-vl-plus', messages=messages)
+        
+        return response.output.choices[0].message.content
+    elif "gpt-4" in model_name:
+        
+        client = OpenAI(api_key='sk-Kf4b9EQQn4z3SzMK05CfB280FdBf4fE0A74b45A7E69f4bA4')
+        #client = OpenAI()
+        messages = [{"role": "user", "content": [{"type": "text", "text": text}]}]
+ 
+        if image_path is not None:
+            #image = Image.open(image_path)
+            base64_image = encode_image(image_path)
+            image_message = {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/png;base64,{base64_image}"}
+            }
+            messages[0]["content"].append(image_message)
     
-    return response.output.choices[0].message.content
+        response = client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            max_tokens=96
+        )
+        return response.choices[0].message.content
+
 
 # Function to encode the image
 def encode_image(image_path):
@@ -122,9 +152,10 @@ def test_internvl():
     
     print(f"平均相似度{average_tanimoto}, tanimoto@1.0为{tanimoto_to_one*100}%")
 
-def test_qwen_vl(restore_qwen_ans_path:str):
+
+def test_online_MLLMs(model_name: str, restore_ans_path:str):
     """
-    test raw Qwen_vl performance
+    test raw online MLLM performance
     """
     tanimoto_list = list()
 
@@ -134,33 +165,31 @@ def test_qwen_vl(restore_qwen_ans_path:str):
     with open('/mnt/petrelfs/zhangdi1/lijunxian/datagen/mm_chem_ocr.jsonl.test.jsonl','r') as fp:
         data_to_test = fp.readlines()
     
-    qwen_vl_smiles_list = list()
+    mllm_smiles_list = list()
 
-    for i in tqdm(range(len(data_to_test)), desc='Calling Qwen-VL to return SMILES'):
+    for i in tqdm(range(len(data_to_test)), desc=f'Calling {model_name} to return SMILES'):
         single_data = json.loads(data_to_test[i])
         img_path = single_data['image']
         #img_path = '/mnt/petrelfs/zhangdi1/lijunxian/dog.jpeg'
         #prompt = "图片上描述了什么"
-        prompt = "你是一位精通化学分子图识别和化学表达式的专家"+single_data['conversations'][0]['value'] + "注意，请只回答图上分子的化学表达式而不回答其他内容。"
+        prompt = "你是一位精通化学分子图识别和SMILES表达式的专家"+single_data['conversations'][0]['value'] + "注意，请仅仅回答图上分子的SMILES表达式而不回答任何其他内容。"
         prompt = prompt.replace('\n','').replace('<image>','')
-        ans_from_qwen = call_qwen_multimodal(img_path, prompt)
-        print(ans_from_qwen)
-        qwen_vl_smiles_list.append(ans_from_qwen)
-        #break
-    return
+        ans_from_mllm = call_multimodal(model_name, img_path, prompt)
+        print(ans_from_mllm)
+        mllm_smiles_list.append(ans_from_mllm)
     
-    with open(restore_qwen_ans_path, 'w', encoding='utf-8') as file:
-        for data in gen_and_gt_list:
+    with open(restore_ans_path, 'w', encoding='utf-8') as file:
+        for data in mllm_smiles_list:
             file.write(json.dumps(data, ensure_ascii=False) + '\n')
     
     f.close()
     fp.close()
     file.close()
     
-    with open(restore_qwen_ans_path,'r') as f:
+    with open(restore_ans_path,'r') as f:
         pred = f.readlines()
     
-    for i in tqdm(range(len(gt)), desc='Calculating Qwen-VL performance'):
+    for i in tqdm(range(len(gt)), desc=f'Calculating {model_name} performance'):
         gt_line = json.loads(gt[i])
         pred_line = json.loads(pred[i])
 
@@ -223,15 +252,15 @@ def test_chemvl_perform_smiles_ocr(ans_paths: list, restore_path: str):
             file.write(json.dumps(data, ensure_ascii=False) + '\n')
 
 if __name__ == "__main__":
-    eval_model_type = 'qwenvl' #internvl, chemvlm or qwenvl
+    eval_model_type = 'gpt-4o' #internvl, chemvlm or qwenvl
     if eval_model_type == 'chemvlm':
         ocr_chemvl_results = ['/mnt/petrelfs/zhangdi1/lijunxian/chemexam_repo/ChemLLM_Multimodal_Exam/results/smiles_ocr_2b_wxz_chemvl_2B_ft_7_3_0_merge_smiles_ocr.jsonl']
         restore_res_path = '/mnt/petrelfs/zhangdi1/lijunxian/qwen_ocr_2b.jsonl'
         test_chemvl_perform_smiles_ocr(ocr_chemvl_results, restore_res_path)
     elif eval_model_type == 'internvl':
         test_internvl()
-    elif eval_model_type == 'qwenvl':
-        qwen_path = '/mnt/petrelfs/zhangdi1/lijunxian/qwen_vl_ocr.jsonl'
-        test_qwen_vl(qwen_path)
+    elif eval_model_type == 'qwen-vl' or eval_model_type == 'gpt-4o':
+        restore_path = '/mnt/petrelfs/zhangdi1/lijunxian/qwen_vl_ocr.jsonl'
+        test_online_MLLMs(eval_model_type, restore_path)
     else:
         raise ValueError(f"Cannot recognize {eval_model_type}")
