299eb552abced8367c74f18d28c1c521df96ca1d
Kyrie <110314639+kyrieLei@users.noreply.github.com>
2024-12-11 12:56:27 +0800

    Update gen_the_cauldron.py

--- datagen/gen_the_cauldron.py
+++ datagen/gen_the_cauldron.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
 
-cauldron = load_dataset('/mnt/hwfile/ai4chem/share/the_cauldron','ai2d')
+cauldron = load_dataset('./the_cauldron','ai2d')
 
 print(cauldron['train'][0]['images'])
