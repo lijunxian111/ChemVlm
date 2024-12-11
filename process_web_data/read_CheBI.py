7561fec0ea10b861fe5a3726baef2d6903285cb7
Kyrie <110314639+kyrieLei@users.noreply.github.com>
2024-12-11 12:36:33 +0800

    Update read_CheBI.py

--- process_web_data/read_CheBI.py
+++ process_web_data/read_CheBI.py
@@ -22,15 +22,15 @@ smiles_templates = [
 ]
 
 smiles_chinese_templates = [
-    "��������ͼƬչʾ�ķ���������ʲô��",
-    "������ӽṹ�������ֻ����",
-    "�ܷ����ʶ��ͼ�е�������ӣ�",
-    "����ͼ���еĻ�ѧ�ṹ��ʲô��",
-    "������ӵĻ�ѧʽ�ܸ�������",
-    "ͼ����ʾ�������ַ��ӣ�",
-    "���ܽ���һ��ͼƬ�з��ӵ������",
-    "����ͼƬ�е�����һ�ֻ�ѧ���ӣ�",
-    "����ʶ���ͼƬ�е����������"]
+    "请问这张图片展示的分子名称是什么？",
+    "这个分子结构代表哪种化合物？",
+    "能否帮我识别图中的这个分子？",
+    "这张图像中的化学结构是什么？",
+    "这个分子的化学式能告诉我吗？",
+    "图中所示的是哪种分子？",
+    "您能解释一下图片中分子的组成吗？",
+    "这张图片中的是哪一种化学分子？",
+    "您能识别出图片中的这个分子吗？"]
 
 
 
@@ -61,16 +61,16 @@ iupac_templates = [
 ]
 
 iupac_chinese_templates = [
-"ͼ�з��ӵ�IUPAC������ʲô��",
-"���ܸ�����ͼ�з��ӵ�IUPAC������",
-"ͼ����ʾ�������IUPAC������ʲô��",
-"���ͼ�з��ӵ�IUPAC��׼������ʲô��",
-"����ͼ����ʾ�ķ��ӵ�IUPAC������ʲô��",
-"ͼ����ʾ���ӵ�ϵͳIUPAC������ʲô��",
-"ͼ�з��ӵ�IUPAC��ʽ������ʲô��",
-"�����ṩͼ�з��ӵ�IUPAC������",
-"ͼ����ʾ���ӵ�IUPAC������ʲô��",
-"ͼ����ʾ�Ļ�ѧ���ӵ�IUPAC�����������ģ�"
+"图中分子的IUPAC名称是什么？",
+"你能告诉我图中分子的IUPAC命名吗？",
+"图中所示化合物的IUPAC名称是什么？",
+"这个图中分子的IUPAC标准名称是什么？",
+"请问图中显示的分子的IUPAC命名是什么？",
+"图中所示分子的系统IUPAC命名是什么？",
+"图中分子的IUPAC正式名称是什么？",
+"你能提供图中分子的IUPAC名称吗？",
+"图中所示分子的IUPAC名称是什么？",
+"图中显示的化学分子的IUPAC命名是怎样的？"
 ]
 
 smiles_answer_templates = english_expressions = [
@@ -107,36 +107,36 @@ smiles_answer_templates = english_expressions = [
 ]
 
 chinese_smiles_answers = [
-    "����Ϊ����ͼƬ��ķ���ʽ����SMILES��ʾӦΪ {}��",
-    "���ҿ�����ͼƬ�еķ��ӽṹ��SMILES��ʽд�� {}��",
-    "�ҵĿ����ǣ���ͼ��ʾ�ķ���ʽ��SMILES��ʾ���� {}��",
-    "�����ҵ����⣬ͼ�еķ���ʽ����SMILES��ʽ��ʾΪ {}��",
-    "�Ҿ��ã����ͼƬ�еĻ�ѧ�ṹ����SMILES������������ {}��",
-    "���Ҹ��˵ĽǶȳ�����ͼƬ��ķ��ӽṹ��SMILES���Ա�ʾΪ {}��",
-    "���ҹ۲죬��ͼ�еķ���ʽ��SMILES��ʽ���ӦΪ {}��",
-    "�ҵĽ���ǣ����ͼ�еķ���ʽ����SMILES��ʽ�� {}��",
-    "���ҵļ��⣬����ͼƬչʾ�ķ���ʽ��SMILES����Ӧ���� {}��",
-    "������˵����ͼƬ�з��ӵ�SMILES��ʾӦ�� {}��",
-    "�ҵķ�����ʾ������ͼ�еķ���ʽ��SMILES��ʽ��ʾΪ {}��",
-    "���ҿ���������ͼƬ�еķ��ӽṹ��SMILES����ʽ�� {}��",
-    "���ҷ�����ͼ��չʾ�ķ���ʽ�����SMILESд����Ӧ�� {}��",
-    "�Ҳ������ͼƬ�ķ���ʽ�������SMILES�﷨��������� {}��",
-    "���ҵĽǶ��жϣ�ͼƬ����ʾ����ʽ��SMILES��ʽ�� {}��",
-    "���Ʋ�����ͼƬ�еķ���ʽ����SMILES��ʽ��������� {}��",
-    "�����ҵĹ۲죬���ͼƬչʾ�ķ���ʽ����SMILES��˵�� {}��",
-    "�ҹ������ͼ�ķ��ӽṹ����SMILES��ʽд������Ӧ���� {}��",
-    "�ҵ�����ǣ���ͼ�ķ��ӽṹ��SMILES��ʽ��ʾ��Ϊ {}��",
-    "���ҿ�����ͼ�еķ���ʽת����SMILES����Ӧ���� {}��",
-    "����֮�������ͼƬ�еķ��ӽṹ��SMILES��ʽ��ʾΪ {}��",
-    "�ҵ������ǣ�ͼ�еķ��ӽṹ����SMILES�����ʾ���� {}��",
-    "���ҵ��ӽǣ���ͼƬ�еķ���ʽ�������SMILES��ʾ������ {}��",
-    "���ҵķ�������������ͼƬ�еķ���ʽ��SMILES�﷨���Ϊ {}��",
-    "�ҵ��ж��ǣ����ͼչʾ�ķ��ӽṹ����SMILES��дӦ�� {}��",
-    "����Ϊ����ͼƬ�еĻ�ѧ�ṹ��ת����SMILES��ʽ������ {}��",
-    "�Ҹо�����ͼ�еķ���ʽ����SMILES�ķ�ʽ��ʾ�������� {}��",
-    "����Ϊ���ͼ���еķ��ӽṹ����SMILES�﷨��ʾ������ {}��",
-    "�ӿ�ѧ���ӽ�����������ͼƬ�еķ���ʽ��SMILES��ǣ�Ӧ�� {}��",
-    "����������ͼƬչʾ�ķ��ӽṹ������SMILES��ʽ��������� {}��"
+    "我认为这张图片里的分子式，用SMILES表示应为 {}。",
+    "在我看来，图片中的分子结构用SMILES格式写作 {}。",
+    "我的看法是，该图显示的分子式的SMILES表示法是 {}。",
+    "根据我的理解，图中的分子式可用SMILES形式表示为 {}。",
+    "我觉得，这个图片中的化学结构，用SMILES来描述，就是 {}。",
+    "从我个人的角度出发，图片里的分子结构用SMILES可以表示为 {}。",
+    "据我观察，此图中的分子式以SMILES形式表达，应为 {}。",
+    "我的解读是，这幅图中的分子式，按SMILES格式是 {}。",
+    "以我的见解，这张图片展示的分子式的SMILES表达应该是 {}。",
+    "对我来说，该图片中分子的SMILES表示应是 {}。",
+    "我的分析显示，这张图中的分子式以SMILES格式表示为 {}。",
+    "在我看来，这张图片中的分子结构的SMILES表达式是 {}。",
+    "依我分析，图中展示的分子式如果用SMILES写出来应是 {}。",
+    "我猜想这幅图片的分子式，如果用SMILES语法来表达，会是 {}。",
+    "从我的角度判断，图片中所示分子式的SMILES形式是 {}。",
+    "我推测这张图片中的分子式，以SMILES格式表达，可能是 {}。",
+    "依据我的观察，这个图片展示的分子式，按SMILES来说是 {}。",
+    "我估计这幅图的分子结构，用SMILES方式写出来，应当是 {}。",
+    "我的意见是，该图的分子结构以SMILES格式表示则为 {}。",
+    "在我看来，图中的分子式转换成SMILES语言应该是 {}。",
+    "依我之见，这个图片中的分子结构，SMILES格式表示为 {}。",
+    "我的理解是，图中的分子结构，用SMILES代码表示，是 {}。",
+    "从我的视角，该图片中的分子式，如果用SMILES表示，会是 {}。",
+    "从我的分析来看，这张图片中的分子式用SMILES语法表达，为 {}。",
+    "我的判断是，这幅图展示的分子结构，用SMILES来写应是 {}。",
+    "我认为这张图片中的化学结构，转换成SMILES格式，就是 {}。",
+    "我感觉这张图中的分子式，以SMILES的方式表示出来，是 {}。",
+    "我认为这幅图表中的分子结构，用SMILES语法表示，就是 {}。",
+    "从科学的视角来看，这张图片中的分子式用SMILES标记，应是 {}。",
+    "我相信这张图片展示的分子结构，采用SMILES格式来表达，将是 {}。"
 ]
 
 caption_answer_templates = [
@@ -175,26 +175,26 @@ iupac_answers = [
 ]
 
 iupac_chinese_answers = [
-    "�Ҿ���ͼ�з��ӵ�IUPAC������ {}",
-"���ҿ�����ͼ�з��ӵ�IUPAC������ {}",
-"����֮����ͼ�з��ӵ�IUPAC������ {}",
-"���ҵĽǶ�������ͼ�з��ӵ�IUPAC������ {}",
-"���Ҷ��ԣ�ͼ�з��ӵ�IUPAC������ {}",
-"�ҵĿ����ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"�ҵĹ۵��ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"�����ţ�ͼ�з��ӵ�IUPAC������ {}",
-"������֪��ͼ�з��ӵ�IUPAC������ {}",
-"����֮����ͼ�з��ӵ�IUPAC������ {}",
-"�͸��˶��ԣ�����Ϊͼ�з��ӵ�IUPAC������ {}",
-"�Ҹ��˵�����ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"�ҵ������ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"���Ʋ�ͼ�з��ӵ�IUPAC������ {}",
-"�ҵĽ����ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"���ҿ�����ͼ�з��ӵ�IUPAC������ {}",
-"�ҳ��еĹ۵��ǣ�ͼ�з��ӵ�IUPAC������ {}",
-"���ҵ�����������ͼ�з��ӵ�IUPAC������ {}",
-"�ҹ���ͼ�з��ӵ�IUPAC������ {}",
-"����Ϊͼ�л������IUPAC������ {}"
+    "我觉得图中分子的IUPAC命名是 {}",
+"在我看来，图中分子的IUPAC命名是 {}",
+"依我之见，图中分子的IUPAC命名是 {}",
+"从我的角度来看，图中分子的IUPAC命名是 {}",
+"对我而言，图中分子的IUPAC命名是 {}",
+"我的看法是，图中分子的IUPAC命名是 {}",
+"我的观点是，图中分子的IUPAC命名是 {}",
+"我相信，图中分子的IUPAC命名是 {}",
+"就我所知，图中分子的IUPAC命名是 {}",
+"依我之见，图中分子的IUPAC名称是 {}",
+"就个人而言，我认为图中分子的IUPAC命名是 {}",
+"我个人的意见是，图中分子的IUPAC命名是 {}",
+"我的理解是，图中分子的IUPAC命名是 {}",
+"我推测图中分子的IUPAC命名是 {}",
+"我的结论是，图中分子的IUPAC命名是 {}",
+"在我看来，图中分子的IUPAC名称是 {}",
+"我持有的观点是，图中分子的IUPAC命名是 {}",
+"从我的立场来看，图中分子的IUPAC命名是 {}",
+"我估计图中分子的IUPAC命名是 {}",
+"我认为图中化合物的IUPAC命名是 {}"
 ]
 
 def add_image_token(text):
@@ -222,7 +222,7 @@ def read_data(path: str):
     #print(len(df.values))
     #print(df['description'].values[0])
     #print(df['choices'].values[0])
-    data = df.to_dict('records') #ת��Ϊ�ֵ��б��Ĺؼ��䣡����
+    data = df.to_dict('records') #转换为字典列表的关键句！！！
 
     """
     for root, _, files in os.walk('/mnt/petrelfs/zhangdi1/lijunxian/CheBI/image'):
@@ -255,8 +255,8 @@ def gen_smiles_dataset(data, img_store_path: str):
         imgs = []
         
         cid_num = line['CID']
-        image_np = cv2.imread('/mnt/petrelfs/zhangdi1/lijunxian/CheBI/image/'+f'CID_{cid_num}.png')
-        #��numpy���󱣴�Ϊjpg��ʽ��ͼƬ�ļ�
+        image_np = cv2.imread('/CheBI/image/'+f'CID_{cid_num}.png')
+        #将numpy矩阵保存为jpg格式的图片文件
         cv2.imwrite(img_store_path + f"{index}.png", image_np)
         imgs.append(img_store_path + f"{index}.png")
         
@@ -286,8 +286,8 @@ def gen_caption_dataset(data, img_store_path: str):
         imgs = []
      
         #image_np = cv2.imread(img_store_path + f"{index}.png")
-        #��numpy���󱣴�Ϊjpg��ʽ��ͼƬ�ļ�
-        #cv2.imwrite(img_store_path + f"{index}.png", image_np) ֻ��Ҫд��һ�μ���
+        #将numpy矩阵保存为jpg格式的图片文件
+        #cv2.imwrite(img_store_path + f"{index}.png", image_np) 只需要写入一次即可
         imgs.append(img_store_path + f"{index}.png")
         
         conversations = {'id': q_id, 'images': imgs, 'conversations':[{'from':'human', 'value': prompt}, {'from':'gpt', 'value': ans_prompt}]}
@@ -320,8 +320,8 @@ def gen_iupac_dataset(data, img_store_path: str):
         imgs = []
      
         #image_np = cv2.imread(img_store_path + f"{index}.png")
-        #��numpy���󱣴�Ϊjpg��ʽ��ͼƬ�ļ�
-        #cv2.imwrite(img_store_path + f"{index}.png", image_np) ֻ��Ҫд��һ�μ���
+        #将numpy矩阵保存为jpg格式的图片文件
+        #cv2.imwrite(img_store_path + f"{index}.png", image_np) 只需要写入一次即可
         imgs.append(img_store_path + f"{index}.png")
         
         conversations = {'id': q_id, 'images': imgs, 'conversations':[{'from':'human', 'value': prompt}, {'from':'gpt', 'value': ans_prompt}]}
@@ -347,15 +347,15 @@ def write_total_data(prompt_list: list, file_path: str):
 if __name__ == "__main__":
     #print('A'+1)
     """
-    raw_data_1 = read_data('/mnt/petrelfs/zhangdi1/ChemQA/data/train-00000-of-00002.parquet')
-    raw_data_2 = read_data('/mnt/petrelfs/zhangdi1/ChemQA/data/train-00001-of-00002.parquet')
+    raw_data_1 = read_data('ChemQA/data/train-00000-of-00002.parquet')
+    raw_data_2 = read_data('ChemQA/data/train-00001-of-00002.parquet')
     raw_data = raw_data_1 + raw_data_2
     """
-    raw_data = read_data('/mnt/petrelfs/zhangdi1/lijunxian/CheBI/test.csv')
-    smiles_prompt_list = gen_smiles_dataset(raw_data, '/mnt/hwfile/ai4chem/share/cheBI/test/')
-    caption_prompt_list = gen_caption_dataset(raw_data, '/mnt/hwfile/ai4chem/share/cheBI/test/')
-    iupac_prompt_list = gen_iupac_dataset(raw_data, '/mnt/hwfile/ai4chem/share/cheBI/test/')
+    raw_data = read_data('CheBI/test.csv')
+    smiles_prompt_list = gen_smiles_dataset(raw_data, 'cheBI/test/')
+    caption_prompt_list = gen_caption_dataset(raw_data, 'cheBI/test/')
+    iupac_prompt_list = gen_iupac_dataset(raw_data, 'cheBI/test/')
 
     total_list = smiles_prompt_list+caption_prompt_list+iupac_prompt_list
-    write_total_data(total_list, '/mnt/petrelfs/zhangdi1/lijunxian/datagen/cheBI_test.jsonl')
-    #convert_bytes_to_images(raw_data, '/mnt/hwfile/ai4chem/share/chemqa/val/', '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chemqa_val.jsonl')
+    write_total_data(total_list, 'datagen/cheBI_test.jsonl')
+    #convert_bytes_to_images(raw_data, 'chemqa/val/', 'datagen/chemqa_val.jsonl')
