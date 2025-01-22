9a614cdad962ae06289b9ea2c50947a4e50d0368
Junxian Li <88318195+lijunxian111@users.noreply.github.com>
2025-01-22 10:14:53 +0800

    Update test_res_openai.py

--- evaluation_tool/eval_files/test_res_openai.py
+++ evaluation_tool/eval_files/test_res_openai.py
@@ -29,21 +29,20 @@ from minicpm_performance import gen_results
 from qwen2_vl_performance import gen_results_qwen_vl
 from deepseek_performance import gen_all_results_deepseek
 
-## Note that "eval_env" environment is needed for this page !!!!!!
 
 
 
 ds_collections = {
     'smiles_ocr':{
         'root': '',
-        'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/mm_chem_ocr.jsonl.test.jsonl',
+        'question': '../../datagen/mm_chem_ocr.jsonl.test.jsonl',
         'prompt': "请根据指令回答一个SMILES化学分子式, 请只回答SMILES化学分子式而不输出其他内容",
         'max_new_tokens': 1000,
         'min_new_tokens': 1,
     },
     'mm_gaokao_test_past': {
         'root': '/mnt/hwfile/ai4chem/share/data',
-        'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/mm_pure_fix.jsonl.test.jsonl',
+        'question': '../../datagen/mm_pure_fix.jsonl.test.jsonl',
         #'prompt': "请判断这道是什么题目并回答, 选择题请只回答A, B, C或D; 填空题请按顺序依次填空,并只回答填入的内容; 主观题请回答问题并给出详细步骤",
         'prompt': "请正确回答这道题目, 选择题请只回答A, B, C或D; 填空题请按顺序依次填空,并只回答填入的内容",
         'max_new_tokens': 1000,
@@ -51,77 +50,77 @@ ds_collections = {
     },
     'CMMU':{
         'root': '',
-        'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/CMMU_test_no_multiple.jsonl',
+        'question': '../../datagen/CMMU_test_no_multiple.jsonl',
         'prompt': "请正确回答这道题目, 选择题请只回答A, B, C或D; 填空题请按顺序依次填空,并只回答填入的内容",
         'max_new_tokens': 1000,
         'min_new_tokens': 1,
     },
     'SciQA':{
         'root': '',
-        'question': '/mnt/petrelfs/zhangdi1/lijunxian/SciQA/sciqa_test.jsonl',
+        'question': '../../SciQA/sciqa_test.jsonl',
         'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
         'max_new_tokens': 1000,
         'min_new_tokens': 1,
     },
     'SciQA_all':{
         'root': '',
-        'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/sciqa_test_all.jsonl',
+        'question': '../../datagen/sciqa_test_all.jsonl',
         'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
         'max_new_tokens': 1000,
         'min_new_tokens': 1,
     },
     'Chembench_mol2cap': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_mol2caption.jsonl',
+       'question': '../../datagen/chembench_mol2caption.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_property': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_property.jsonl',
+       'question': '../../datagen/chembench_property.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_name_conv': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_name_conv.jsonl',
+       'question': '../../datagen/chembench_name_conv.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_retro': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_retro.jsonl',
+       'question': '../../datagen/chembench_retro.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_temperature': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_temperature.jsonl',
+       'question': '../../datagen/chembench_temperature.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_solvent': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_solvent.jsonl',
+       'question': '../../datagen/chembench_solvent.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chembench_yield': {
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/chembench_yield.jsonl',
+       'question': '../../datagen/chembench_yield.jsonl',
        'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
        'max_new_tokens': 1000,
        'min_new_tokens': 1,
     },
     'Chebi_caption':{
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/cheBI_caption_eng_test.jsonl',
+       'question': '../../datagen/cheBI_caption_eng_test.jsonl',
        'prompt': "Please follow the question and give your answer. Answer briefly in English. Here is one example answer: \n'''It seems to me the molecule is an {} in which {}. It has a role as an {}. It is an {}, {}, {}. It derives from a {}.'''\n. Follow this format.",
        'max_new_tokens': 512,
        'min_new_tokens': 1,
@@ -156,65 +155,23 @@ ds_collections = {
     },
     'orderly_type':{
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/orderly_test_type.jsonl',
+       'question': '../../datagen/orderly_test_type.jsonl',
        'prompt': "Please follow the question and give your answer. Choose one from '''{}'''. Remember that only return several words of the reaction type and do not return explanations.",
        'max_new_tokens': 200,
        'min_new_tokens': 1,
     },
     'pistachio_type_sample':{
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/pistachio_mm_test_1000.jsonl',
+       'question': '../../datagen/pistachio_mm_test_1000.jsonl',
        'prompt': "Please follow the question and give your answer. Choose one from '''{}'''. Remember that only return several words of the reaction type and do not return explanations.",
        'max_new_tokens': 200,
        'min_new_tokens': 1,
     },
     'orderly_product':{
        'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/orderly_test_project.jsonl',
+       'question': '../../datagen/orderly_test_project.jsonl',
        'prompt': "Please answer a SMILES string representing the molecule only, and do not answer any other things.",
        'max_new_tokens': 200,
-       'min_new_tokens': 1,
-    },
-    'math_verse':{
-       'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/math_verse_test.jsonl',
-       'prompt': "Your answer format should be ```{Answer: }```",
-       'max_new_tokens': 100,
-       'min_new_tokens': 1,
-    },
-    'seed':{
-       'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/seed_test.jsonl',
-       'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
-       'max_new_tokens': 100,
-       'min_new_tokens': 1,
-    },
-    'mmtbench':{
-       'root': '',
-       'question': '/mnt/petrelfs/zhangdi1/lijunxian/datagen/mmtbench_val.jsonl',
-       'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
-       'max_new_tokens': 100,
-       'min_new_tokens': 1,
-    },
-    'mmbench':{
-       'root': '',
-       'question': '/mnt/hwfile/ai4chem/wangweida/data/MMBench/mmbench_dev.jsonl',
-       'prompt': "Please answer A, B, C or D according to the question. You should only answer a capital letter.",
-       'max_new_tokens': 100,
-       'min_new_tokens': 1,
-    },
-    'realworldqa':{
-       'root': '',
-       'question': '/mnt/hwfile/ai4chem/CVPR/realworldqa/json/generated_data.jsonl',
-       'prompt': "Please answer the question.",
-       'max_new_tokens': 100,
-       'min_new_tokens': 1,
-    },
-    'math_vista':{
-       'root': '',
-       'question': '/mnt/hwfile/ai4chem/wangweida/data/MathVista/mathvista_testmini.jsonl',
-       'prompt': "Your answer format should be ```{Answer: }```",
-       'max_new_tokens': 100,
        'min_new_tokens': 1,
     },
     
@@ -226,13 +183,13 @@ import requests
 ss = requests.session()
 ss.keep_alive = False
 
-dashscope.api_key = 'sk-eea7c876461747c5a6eebe0531164767' #qwen API-KEY
+dashscope.api_key = 'sk-' #qwen API-KEY
 
 
-openai.api_key = 'sk-AFgqhX1yg2X0L8DTF068CeCdDa3b4082A64fB96cBa7aF9D8'
+openai.api_key = 'sk-'
 
 
-with open('/mnt/petrelfs/zhangdi1/lijunxian/eval_results/gpt-4o-pistachio_type_sample.jsonl', 'r', encoding='utf8') as f:
+with open('../../eval_results/gpt-4o-pistachio_type_sample.jsonl', 'r', encoding='utf8') as f:
     type_data = f.readlines()
 
 f.close()
@@ -273,10 +230,9 @@ def call_multimodal(model_name, image_path, text):
 
         return response.output.choices[0].message.content
     elif "gpt" in model_name:
-        #client = OpenAI(api_key='sk-kHGxnk8OptzWhewzmcA0TCuLd2XtR8gXKqEevcUhQ0D9yJYs', base_url="https://api.claudeshop.top/v1")   #CVPR
-        client = OpenAI(api_key='sk-CDgTXzUtECTUk5OIo8tsE00XF5kmyzM7DWSJdfdgLfvY2lKr', base_url="https://api.claudeshop.top/v1")  #ChemVLM
-        #client = OpenAI(api_key='sk-AFgqhX1yg2X0L8DTF068CeCdDa3b4082A64fB96cBa7aF9D8', base_url="https://api.claudeshop.top/v1")
-        #client = OpenAI(api_key='sk-proj-aWWD7RfhrndOFwvwCkSCT3BlbkFJIhYOggcmq0Oqq8pJ1riM')
+        client = OpenAI(api_key='sk-', base_url="https://api.claudeshop.top/v1")  #ChemVLM
+        #client = OpenAI(api_key='sk-', base_url="https://api.claudeshop.top/v1")
+        #client = OpenAI(api_key='sk-')
         messages = [{"role": "user", "content": [{"type": "text", "text": text}]}]
 
         text = text.replace('<image>','')
@@ -315,8 +271,8 @@ def call_multimodal(model_name, image_path, text):
         return response.choices[0].message.content
     
     elif "yi" in model_name:
-        client = OpenAI(api_key='0832fa4dbe0642efbd01ebcee4debe01', base_url="https://api.lingyiwanwu.com/v1")
-        #client = OpenAI(api_key='58073c26cafa4775b7449769ee3bd5c7', base_url="https://api.lingyiwanwu.com/v1")
+        client = OpenAI(api_key='', base_url="https://api.lingyiwanwu.com/v1")
+        #client = OpenAI(api_key='', base_url="https://api.lingyiwanwu.com/v1")
         messages = [{"role": "user", "content": [{"type": "text", "text": text}]}]
 
         if image_path is not None:
@@ -341,7 +297,7 @@ def call_multimodal(model_name, image_path, text):
     
     elif 'gemini' in model_name:
 
-        genai.configure(api_key='FBY245kK7Ye10kPiE3ME48T6vXxgcAtnMO8jhdSK9Qe4SdG0')
+        genai.configure(api_key='')
         image = Image.open(image_path)
         image_base64 = image_to_base64(image_path, for_image_url=False)
         try:
@@ -377,7 +333,7 @@ def call_multimodal(model_name, image_path, text):
     
     elif "glm" in model_name:
 
-        client = ZhipuAI(api_key="06cfd38846f9f99134979697c6077e5f.ZXIRvlPKhSLDKdLl") # APIKey
+        client = ZhipuAI(api_key="") # APIKey
         base64_image = encode_image(image_path)
         try:
             img_type = get_image_extension(image_path)[1:]
@@ -467,7 +423,6 @@ def get_answer(model_name, dataset_name, q_path, restore_file_path):
 
 if __name__ == "__main__":
     #TODO: CMMU + Yi
-    #call_multimodal('gemini-1.5-pro', '/mnt/hwfile/ai4chem/share/chemqa/test/7.jpg', 'What is in this picture?')
     #dataset_names = ["Chembench_temperature","Chembench_retro","Chembench_solvent","Chembench_yield"]
     #dataset_names = ["chirality_yon","chirality_num","molecule"]
     dataset_names = ['pistachio_type_sample']
@@ -509,7 +464,7 @@ if __name__ == "__main__":
 
     if "llava" in model_names:
         
-        command = "srun -p AI4Phys --gres=gpu:1 python /mnt/petrelfs/zhangdi1/lijunxian/LLaVA/vqn_chem.py"
+        command = "srun -p AI4Phys --gres=gpu:1 python ../../LLaVA/vqn_chem.py"
         args = shlex.split(command)
         process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         while True:
@@ -520,7 +475,7 @@ if __name__ == "__main__":
                 print(output.strip())
         rc = process.poll()
         model_names.remove('llava')
-        #generate_answers("/mnt/petrelfs/zhangdi1/lijunxian/LLaVA/llava-v1.5-13b", ds_collections[dataset_name]['question'], ds_collections[dataset_name]['question']['root'], f'./{model_name}-{dataset_name}.jsonl')
+        #generate_answers("../../LLaVA/llava-v1.5-13b", ds_collections[dataset_name]['question'], ds_collections[dataset_name]['question']['root'], f'./{model_name}-{dataset_name}.jsonl')
     #else:
     if len(model_names)!=0:
         for model_name in model_names:
